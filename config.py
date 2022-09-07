d794e899ea47a362b0c10c8806fe63fd5301f163
sunzhengyu99 <sunzy9901@qq.com>
2022-09-07 23:20:33 +0800

    添加：生成抢课功能

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 # author: fe1w0
 
-user_id = ""
-password = ""
+user_id = "22151214335"
+password = "2632338423.."
+
+course_KCDM = "X1HA0001"            # 需要抢课的课程代码
+
+sleep_time = 60 # 刷新的频率60s，可以根据需要更改
